1be63dae5777e6be4c4e6cc383b9c3f7a293866b
Ba6ai <eliseimartelov292@gmail.com>
2026-03-10 17:14:42 +0500

    ex1

---
+++ ex1/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+// Создание списка
+let rec numberss(currentlist: int list) : int list = 
+    let x = Console.ReadLine()
+    if x = "ex" then
+        currentlist
+    else
+        numberss(currentlist @ [int(x)])
+
+// Вычисление суммы списка
+let sumCount n = 
+    let rec loop curr summ =
+        if curr = 0 then summ
+        else loop (curr/10) (summ + (curr%10))
+    loop n 0 
+
+let sumList num = 
+    num |> Seq.map sumCount |> Seq.toList
+
+[<EntryPoint>]
+let main _ =
+    printfn "Введите числа (через Enter), для выхода введите 'ex':"
+    let res = numberss []
+    let result = sumList res
+
+    printfn "Исходный список: %A" res
+    printfn "Суммы цифр: %A" result
+    0
