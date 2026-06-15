41097cafb9980e7adc1a5673c63364d3d304792b
Ba6ai <153833234+Ba6ai@users.noreply.github.com>
2026-06-16 01:29:39 +0500

    подготовка ех3

--- ex3/Program.fs
+++ ex3/Program.fs
@@ -3,22 +3,22 @@ open System.IO
 
 let symbol() = 
     printf "Введите символ для поиска файлов: "
-    Console.ReadLine()
+    Console.ReadLine()[0] // Сразу берёт первый символ из введённой строки
     
 [<EntryPoint>]
 let main _ =
-    let path = @"C:\Users\elise\OneDrive\УНИК\2Курс\2\ЯП\lab3\New_fold"
-    let s = symbol()
+    let path = "C:\\Users\\elise\\OneDrive\\УНИК\\2Курс\\2\\ЯП\\kt1\\lab3\\New_fold"
+    let target = symbol()
 
     if Directory.Exists(path) then // Проверка существования папки
         let files = 
-            Directory.EnumerateFiles(path) // Получение последовательности путей, вадыёт по одному
-
+            Directory.EnumerateFiles(path) // Возвращает ленивую последовательность путей
             |> Seq.map Path.GetFileName // Преобразует целый путь в короткое имя
-            |> Seq.filter (fun name -> name.[0] = s.[0]) // Ищет файлы на введённую букву
-            |> Seq.toList  // Превращение в список
+            |> Seq.filter (fun name ->
+                name.Length > 0 && name[0] = target) // Ищет файлы на введённую букву
+            |> Seq.toList  // Жадно собирает результаты в итоговый список
         
-        printfn "Список файлов на букву %s: %A " s files
+        printfn "Список файлов на букву %c: %A " target files
     else
         printfn "Путь не верен"
         
